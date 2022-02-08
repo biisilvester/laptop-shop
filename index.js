0cfad3964531f65e4d225c1d46fc3a6ffdc3522b
biisilvester <biisilvester1996@gmail.com>
2022-02-08 19:55:56 +0300

    Intial commit

--- index.js
+++ index.js
@@ -54,7 +54,12 @@ const server = http.createServer((req, res) => {
         res.end(output);
       }
     );
-else if(){}
+  } else if (/\.(jpg|jpeg|png|gif|)$/i.test(pathName)) {
+    fs.readFile(`${__dirname}/data/img${pathName}`, (err, data) => {
+      res.writeHead(200, { "Content-type": "image/jpg" });
+      res.end(data);
+    });
+
     //URL NOT FOUND
   } else {
     res.writeHead(404, { "Content-type": "text/html" });
@@ -74,9 +79,9 @@ function replaceTemplate(oringinalHtml, laptop) {
   output = output.replace(/{%SCREEN%}/g, laptop.screen);
   output = output.replace(/{%CPU%}/g, laptop.cpu);
   output = output.replace(/{%STORAGE%}/g, laptop.storage);
-  output = output.replace(/{%RAM%}/, laptop.ram);
+  output = output.replace(/{%RAM%}/g, laptop.ram);
   output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
-  output = output.replace(/{%ID%}/, laptop.id);
+  output = output.replace(/{%ID%}/g, laptop.id);
 
   return output;
 }
